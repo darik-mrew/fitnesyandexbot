649ef518939114937993a6089c0711a8ecf46ebe
Dasha <darac0816@gmail.com>
2024-04-27 13:21:44 +0300

    V1.4

--- bot_func.py
+++ bot_func.py
@@ -20,79 +20,65 @@ def create_user(user_id):
 
 
 def add_helper_to_message(text):
-    text += '\n\nОтправте сообщение /commands или /команды, чтобы увидеть список всех возможных команд бота!'
+    text += '\n\nОтправьте сообщение /commands или /команды, чтобы увидеть список всех возможных команд бота!'
     return text
 
 
 @bot.message_handler(commands=['start', 'старт'])
 def send_welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    button = types.KeyboardButton("Начнем!")
-    markup.add(button)
+    tennis_button = types.KeyboardButton("Теннис")
+    volleyball_button = types.KeyboardButton("Волейбол")
+    basketball_button = types.KeyboardButton("Баскетбол")
+    football_button = types.KeyboardButton("Футбол")
+    fitness_button = types.KeyboardButton("Фитнес")
+    swimming_button = types.KeyboardButton("Плавание")
+    markup.add(tennis_button, volleyball_button, basketball_button, football_button, fitness_button, swimming_button)
 
     user_name = message.from_user.username
     bot.send_message(message.chat.id,
-                     f'Приветствую тебя, {user_name}! Я бот, созданный, чтобы помочь тебе в спорте. Начнем?',
+                     f'Приветствую тебя, {user_name}! Я бот, созданный, чтобы помочь тебе в спорте. '
+                     f'Выбери вид спорта, который тебе интересен.',
                      reply_markup=markup)
 
 
-# не забывать добавлять сюда новые команды
-@bot.message_handler(commands=['команды', 'commands'])
+@bot.message_handler(commands=['commands', 'команды'])
 def send_commands(message):
-    possible_commands = {'/commands или /команды': 'выводит список список всех команд бота.'}
+    possible_commands = {'/commands или /команды': 'выводит список всех команд бота.'}
 
     text = '\n'.join([f'{key} - {possible_commands[key]}' for key in possible_commands.keys()])
     bot.send_message(message.chat.id, text)
 
 
-@bot.message_handler(commands=['sport_results', 'спортивные результаты'])
-def send_all_sport_results(message):
+@bot.message_handler(func=lambda message: message.text in ['теннис', 'волейбол', 'баскетбол', 'футбол', 'фитнес', 'плавание'])
+def handle_sport_buttons(message):
+    chosen_sport = message.text
+    if chosen_sport == 'теннис':
+        bot.send_message(message.chat.id, 'Мячи для тенниса', reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(text="Купить", url="https://www.ozon.ru/category/tennisnye-myachi-11278/")))
+    elif chosen_sport == 'волейбол':
+        bot.send_message(message.chat.id, 'Мяч для волейбола', reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(text="Купить", url="https://www.ozon.ru/category/voleybolnye-myachi-11258/")))
+    elif chosen_sport == 'баскетбол':
+        bot.send_message(message.chat.id, 'Мяч для баскетбола', reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(text="Купить", url="https://www.ozon.ru/category/basketbolnye-myachi-11253/")))
+    elif chosen_sport == 'футбол':
+        bot.send_message(message.chat.id, 'Мяч для футбола', reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(text="Купить", url="https://www.ozon.ru/category/futbolnye-myachi-11277/")))
+    elif chosen_sport == 'фитнес':
+        bot.send_message(message.chat.id, 'Гантели для фитнеса', reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(text="Купить", url="https://www.ozon.ru/category/ganteli-11641/")))
+    elif chosen_sport == 'плавание':
+        bot.send_message(message.chat.id, 'Шапочки для плавания', reply_markup=types.InlineKeyboardMarkup().add(types.InlineKeyboardButton(text="Купить", url="https://www.ozon.ru/category/shapochki-dlya-plavaniya-11184/")))
+
+
+def add_sport_to_user_data(user_id, chosen_sport):
     db_sess = db_session.create_session()
-
-    user_id = int(message.chat.id)
-    user = db_sess.query(User).filter(User.id == user_id).one()
-
-    sport_results = pickle.loads(user.best_sport_results)
-    if len(sport_results.keys()) == 0:
-        text = 'У вас пока что не записаны никакие спортивные результаты!'
-    else:
-        text = '\n'.join([f'{exercise}: {sport_results[exercise]}' for exercise in sport_results.keys()])
-
-    bot.send_message(message.chat.id, add_helper_to_message(text))
-
-
-@bot.message_handler(commands=['write_result', 'записать результат'])
-def write_sport_result(message):
-    print(dir(message.date))
-    print(message.date.real)
-    print(message.date.conjugate)
-    print(message.date.from_bytes)
-    print(message.date.to_bytes)
-
-    print(message.date)
-    print(message.text)
+    user = db_sess.query(User).filter(User.id == user_id).first()
+    user.chosen_sport = chosen_sport
+    db_sess.commit()
 
 
 @bot.message_handler(func=lambda message: True)
-def handle_button_click(message):
-    if message.text == "Начнем!":
-        user_id = message.chat.id
-        db_sess = db_session.create_session()
-        if user_id in [user.id for user in db_sess.query(User).all()]:
-            text = "Вы уже зарегистрированы в системе!"
-        else:
-            create_user(user_id)
-            text = "Вы успешно зарегистрированы в системе!"
-
-        markup = types.ReplyKeyboardRemove()
-        bot.reply_to(message, add_helper_to_message(text), reply_markup=markup)
-
-
-# добавить функцию add_helper_to_message при необходимости
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, "Привет, нужна помощь?")
+def handle_message(message):
+    user_id = message.chat.id
+    chosen_sport = message.text
+    add_sport_to_user_data(user_id, chosen_sport)
 
 
 db_session.global_init("db/data_base.db")
